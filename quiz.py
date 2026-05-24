fa78ff1240d9d3369fe3a4b019190a7804ad1e0a
Shagun-2803 <contactshagunchaudhari@gmail.com>
2026-05-24 14:21:58 +0530

    Created starting point for project

---
+++ quiz.py
@@ -0,0 +1,12 @@
+print("Welcome to this quix game!")
+
+print("Rules of this quiz game are: ")
+print("1. Each questions has 4 options and 1 is the correct answer.")
+print("2. Choose only one option by writing the option letter as only one answer exists.")
+print("3. Each correct option gives only one point.")
+
+print('Click Enter to start')
+
+name = input("Enter your name: ")
+print(f"Hello, ${name}.\nPress Enter to Start.")
+
